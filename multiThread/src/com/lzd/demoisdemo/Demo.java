29ba0955c031ff28f8337e0d6cd0622630bf3276
liudong <java_liudong@163.com>
2016-08-05 19:22:20 +0800

    连接示例

---
+++ multiThread/src/com/lzd/demoisdemo/Demo.java
@@ -0,0 +1,77 @@
+package com.lzd.demoisdemo;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.logging.Logger;
+
+import org.junit.Test;
+
+/**
+ * 编写一些不记得的使用类
+ * @date 2016年8月4日
+ * @author lzd
+ *
+ */
+public class Demo {
+
+	private final static Logger log = Logger.getLogger(Demo.class.getName());
+	
+	
+	@Test
+	public void testStringBuffer(){
+		StringBuffer sb = new StringBuffer();
+		/*sb.insert(0, "刘泽栋", sb.length(), "刘泽栋".length());
+		sb.insert(0, "张学友", sb.length(), "张学友".length());
+		sb.insert(0, "lzd", sb.length(), "lzd".length());
+		sb.insert(0, "刘华", sb.length(), "刘华".length());*/
+		sb.append("1").append(",")
+		.append("2").append(",")
+		.append("3").append(",")
+		.append("4").append(",")
+		.append("5").append(",");
+		
+		sb.delete(sb.length() - 1, sb.length());
+		sb.delete(sb.indexOf(","), sb.length());
+		
+		System.out.println(sb);
+	}
+	
+	/**
+	 * 多请求
+	 * 
+	 * @author 刘泽栋 2016年8月4日 下午3:48:17
+	 */
+	@Test
+	public void moreConnection(){
+		String url = "http://localhost:2222";
+		try {
+			URL u = new URL(url);
+			URLConnection uc = u.openConnection();
+			Reader r = new InputStreamReader(new BufferedInputStream(uc.getInputStream()), "UTF-8");
+			
+			StringBuffer response = new StringBuffer();
+			while(true){
+				int c;
+				if((c = r.read()) != -1){
+					response.append((char) c);
+				}else {
+					break;
+				}
+			}
+			log.info(response.toString());
+			
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+				
+	}
+	
+	
+}
