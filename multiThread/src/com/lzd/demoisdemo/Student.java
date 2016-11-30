9ef4b5b5bd800d414b6378a98940f3fc8942b3db
liudong <java_liudong@163.com>
2016-11-30 11:04:18 +0800

    提交测试学生类

---
+++ multiThread/src/com/lzd/demoisdemo/Student.java
@@ -0,0 +1,24 @@
+package com.lzd.demoisdemo;
+
+import java.util.Date;
+
+
+public class Student {
+
+	
+	private String username;
+	private Date date;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+}
